9c6bd719485659c2c2a49ccb1c78e0e7594baaa4
Lop <lopzac981@gmail.com>
2025-01-28 10:23:36 -0500

    Increment Release number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 100
-        versionName = "1.0.0.1"
+        versionName = "1.0.0.2"
 
         kapt {
             arguments {
