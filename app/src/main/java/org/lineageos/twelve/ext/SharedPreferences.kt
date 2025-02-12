e472d9068d7fabbaba47c2624b6c2fe7043b5ce7
Lop <lopzac981@gmail.com>
2025-02-11 19:09:14 -0500

    Disable offload by default
    Also disable "Stop Playback on Task Removed" by default.

--- app/src/main/java/org/lineageos/twelve/ext/SharedPreferences.kt
+++ app/src/main/java/org/lineageos/twelve/ext/SharedPreferences.kt
@@ -42,7 +42,7 @@ fun <T> SharedPreferences.preferenceFlow(
 
 // Generic prefs
 const val ENABLE_OFFLOAD_KEY = "enable_offload"
-private const val ENABLE_OFFLOAD_DEFAULT = true
+private const val ENABLE_OFFLOAD_DEFAULT = false
 var SharedPreferences.enableOffload: Boolean
     get() = getBoolean(ENABLE_OFFLOAD_KEY, ENABLE_OFFLOAD_DEFAULT)
     set(value) = edit {
@@ -58,7 +58,7 @@ var SharedPreferences.enableFloatOutput: Boolean
     }
 
 private const val STOP_PLAYBACK_ON_TASK_REMOVED_KEY = "stop_playback_on_task_removed"
-private const val STOP_PLAYBACK_ON_TASK_REMOVED_DEFAULT = true
+private const val STOP_PLAYBACK_ON_TASK_REMOVED_DEFAULT = false
 var SharedPreferences.stopPlaybackOnTaskRemoved: Boolean
     get() = getBoolean(STOP_PLAYBACK_ON_TASK_REMOVED_KEY, STOP_PLAYBACK_ON_TASK_REMOVED_DEFAULT)
     set(value) = edit {
