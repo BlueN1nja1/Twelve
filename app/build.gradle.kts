32c1415e910c1b9230fb7a31b50c112334fe9c22
Lop <55638449+BlueN1nja1@users.noreply.github.com>
2025-02-22 13:21:58 -0500

    Update to 1.0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 100
-        versionName = "1.0.0.2"
+        versionName = "1.0.0.3"
 
         kapt {
             arguments {
