8b00a270a635c4cf87b842ebf4c699ba4a54347e
Lop <lopzac981@gmail.com>
2025-03-15 22:06:21 -0400

    Update to 1.0.0.4
    Also lower the Minimum SDK to Android 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,10 +21,10 @@ android {
 
     defaultConfig {
         applicationId = "org.lineageos.twelve"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 35
         versionCode = 100
-        versionName = "1.0.0.3"
+        versionName = "1.0.0.4"
 
         kapt {
             arguments {
